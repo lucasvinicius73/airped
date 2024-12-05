525c5ea7bf1af6d86f7932da5c431b17d4510cfd
Lucas Vinicius <lucasidelfonso73@gmail.com>
2024-12-05 17:12:42 -0300

    Adicionando Logos na Home

--- lib/home/new_home_page.dart
+++ lib/home/new_home_page.dart
@@ -27,145 +27,167 @@ class NewHomePage extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                Column(
-                  children: [
-                    const Text(
-                      "PROCEDIMENTOS",
-                      style: TextStyle(
-                        color: Color(0xFF1C72C2),
-                        fontSize: 34,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w700,
-                        height: 0,
-                      ),
+                const Text(
+                  "PROCEDIMENTOS",
+                  style: TextStyle(
+                    color: Color(0xFF1C72C2),
+                    fontSize: 34,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w700,
+                    height: 0,
+                  ),
+                ),
+                const SizedBox(
+                  height: 22,
+                ),
+                const NewMenuButton(
+                  title: "Volume Corrente e Frequência Respiratória",
+                  route: "/volume_corrente_page",
+                  icon: 'assets/home/pulmao.png',
+                  color: Color(0xFF6AC7DE),
+                  index: 1,
+                ),
+                const NewMenuButton(
+                  title: "Tubo Orotraqueal(TOT) e\nFixação do TOT",
+                  route: "/tot_pf_page",
+                  icon: 'assets/home/tot.png',
+                  color: Color(0xFFEBEC95),
+                  index: 2,
+                ),
+                const NewMenuButton(
+                  title: "Pronga de CPAP Nasal e\nCânula de Traqueostomia(TQT)",
+                  route: "/cpap_tqt_page",
+                  icon: 'assets/home/tqt.png',
+                  color: Color(0xFFF9C4E3),
+                  index: 3,
+                ),
+                const NewMenuButton(
+                  title: "Avaliação Respiratória",
+                  route: "/desconforto_resp_page",
+                  icon: 'assets/home/baby.png',
+                  color: Color(0xFFBDDD80),
+                  index: 4,
+                ),
+                const NewMenuButton(
+                  title: "Parâmetros \nVentilatórios Iniciais",
+                  route: "/parametros_page",
+                  icon: 'assets/home/pc.png',
+                  color: Color(0xFFD6A0F5),
+                  index: 5,
+                ),
+                Container(
+                  height: 160,
+                  constraints: const BoxConstraints(maxWidth: 400),
+                  padding: const EdgeInsets.only(top: 19, bottom: 33),
+                  decoration: ShapeDecoration(
+                    color: const Color(0xFFFBFAFA),
+                    shape: RoundedRectangleBorder(
+                      side:
+                          const BorderSide(width: 1, color: Color(0x7FF1F5F4)),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    const SizedBox(
-                      height: 22,
-                    ),
-                    const NewMenuButton(
-                      title: "Volume Corrente e Frequência Respiratória",
-                      route: "/volume_corrente_page",
-                      icon: 'assets/home/pulmao.png',
-                      color: Color(0xFF6AC7DE),
-                      index: 1,
-                    ),
-                    const NewMenuButton(
-                      title: "Tubo Orotraqueal(TOT) e\nFixação do TOT",
-                      route: "/tot_pf_page",
-                      icon: 'assets/home/tot.png',
-                      color: Color(0xFFEBEC95),
-                      index: 2,
-                    ),
-                    const NewMenuButton(
-                      title:
-                          "Pronga de CPAP Nasal e\nCânula de Traqueostomia(TQT)",
-                      route: "/cpap_tqt_page",
-                      icon: 'assets/home/tqt.png',
-                      color: Color(0xFFF9C4E3),
-                      index: 3,
-                    ),
-                    const NewMenuButton(
-                      title: "Avaliação Respiratória",
-                      route: "/desconforto_resp_page",
-                      icon: 'assets/home/baby.png',
-                      color: Color(0xFFBDDD80),
-                      index: 4,
-                    ),
-                    const NewMenuButton(
-                      title: "Parâmetros \nVentilatórios Iniciais",
-                      route: "/parametros_page",
-                      icon: 'assets/home/pc.png',
-                      color: Color(0xFFD6A0F5),
-                      index: 5,
-                    ),
-                    Container(
-                      height: 160,
-                      constraints: const BoxConstraints(maxWidth: 400),
-                      padding: const EdgeInsets.only(top: 19, bottom: 33),
-                      decoration: ShapeDecoration(
-                        color: const Color(0xFFFBFAFA),
-                        shape: RoundedRectangleBorder(
-                          side: const BorderSide(
-                              width: 1, color: Color(0x7FF1F5F4)),
-                          borderRadius: BorderRadius.circular(10),
+                    shadows: const [
+                      BoxShadow(
+                        color: Color(0x19000000),
+                        blurRadius: 4,
+                        offset: Offset(0, 4),
+                        spreadRadius: 0,
+                      )
+                    ],
+                  ),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Informações",
+                          style: TextStyle(
+                            color: Color(0xFF1C72C2),
+                            fontSize: 34,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w700,
+                            height: 0,
+                          ),
                         ),
-                        shadows: const [
-                          BoxShadow(
-                            color: Color(0x19000000),
-                            blurRadius: 4,
-                            offset: Offset(0, 4),
-                            spreadRadius: 0,
-                          )
-                        ],
-                      ),
-                      child: Center(
-                        child: Column(
-                          children: [
-                            const Text(
-                              "Informações",
-                              style: TextStyle(
-                                color: Color(0xFF1C72C2),
-                                fontSize: 34,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.w700,
-                                height: 0,
+                        const Spacer(),
+                        InkWell(
+                          onTap: () {
+                            drawerController.changeIndex(6);
+                            Navigator.of(context).pushNamed('/about');
+                          },
+                          child: Container(
+                            width: 187,
+                            height: 47,
+                            decoration: ShapeDecoration(
+                              color: const Color(0xFFFBFAFA),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(100),
                               ),
+                              shadows: const [
+                                BoxShadow(
+                                  color: Color(0x3F000000),
+                                  blurRadius: 4,
+                                  offset: Offset(0, 4),
+                                  spreadRadius: 0,
+                                )
+                              ],
                             ),
-                            const Spacer(),
-                            InkWell(
-                              onTap: () {
-                                drawerController.changeIndex(6);
-                                Navigator.of(context).pushNamed('/about');
-                              },
-                              child: Container(
-                                width: 187,
-                                height: 47,
-                                decoration: ShapeDecoration(
-                                  color: const Color(0xFFFBFAFA),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(100),
+                            child: const Row(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.help_outline,
+                                  color: Color(0xFF67ABEB),
+                                  size: 22,
+                                ),
+                                SizedBox(
+                                  width: 7,
+                                ),
+                                Text(
+                                  'Sobre',
+                                  style: TextStyle(
+                                    color: Color(0xFFEFCA30),
+                                    fontSize: 20,
+                                    fontFamily: 'Roboto',
+                                    fontWeight: FontWeight.w600,
+                                    height: 0,
                                   ),
-                                  shadows: const [
-                                    BoxShadow(
-                                      color: Color(0x3F000000),
-                                      blurRadius: 4,
-                                      offset: Offset(0, 4),
-                                      spreadRadius: 0,
-                                    )
-                                  ],
                                 ),
-                                child: const Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                      Icons.help_outline,
-                                      color: Color(0xFF67ABEB),
-                                      size: 22,
-                                    ),
-                                    SizedBox(
-                                      width: 7,
-                                    ),
-                                    Text(
-                                      'Sobre',
-                                      style: TextStyle(
-                                        color: Color(0xFFEFCA30),
-                                        fontSize: 20,
-                                        fontFamily: 'Roboto',
-                                        fontWeight: FontWeight.w600,
-                                        height: 0,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
+                              ],
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                  ],
-                )
+                  ),
+                ),
+                const SizedBox(
+                  height: 22,
+                ),
+                Container(
+                  constraints: const BoxConstraints(maxWidth: 300),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: 80,
+                        width: 75,
+                        child: Image.asset("assets/about/brasaouepa.png"),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        height: 80,
+                        width: 75,
+                        child: Image.asset("assets/about/logoCipe.png"),
+                      ),
+                      const Spacer(),
+                      SizedBox(
+                        height: 80,
+                        width: 75,
+                        child: Image.asset("assets/about/logoCapes.png"),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
