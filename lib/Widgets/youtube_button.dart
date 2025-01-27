bdb6fccefa6fdda0cd4ebfd8dc36439405826b9b
Lucas Vinicius <lucasidelfonso73@gmail.com>
2025-01-27 16:19:58 -0300

    Configurando o XML para o Android abrir link

--- lib/Widgets/youtube_button.dart
+++ lib/Widgets/youtube_button.dart
@@ -11,44 +11,57 @@ class YoutubeButton extends StatelessWidget {
     return Material(
       color: Colors.transparent,
       child: InkWell(
-        onTap: () => _launchYouTube(),
+        onTap: () => _launchYouTube(context, link),
         borderRadius: BorderRadius.circular(20),
-        child: Container(
-          width: 130,
-          height: 46,
-          padding: const EdgeInsets.symmetric(horizontal: 13),
-          decoration: CustomDecoration.shapeDecoration,
-          child: const Row(
-            children: [
-              Icon(Icons.play_arrow),
-              Text(
-                'Acesse',
-                style: TextStyle(
-                  color: Color(0xFF67ABEB),
-                  fontSize: 20,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w600,
-                  height: 0,
+        child: Ink(
+          child: Container(
+            width: 130,
+            height: 46,
+            padding: const EdgeInsets.symmetric(horizontal: 13),
+            decoration: CustomDecoration.shapeDecoration,
+            child: Row(
+              children: const [
+                Icon(Icons.play_arrow),
+                SizedBox(width: 8), // Espaço entre o ícone e o texto
+                Text(
+                  'Acesse',
+                  style: TextStyle(
+                    color: Color(0xFF67ABEB),
+                    fontSize: 20,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w600,
+                    height: 1.2,
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void _launchYouTube() async {
-    final Uri url = Uri.parse(link);
+  Future<void> _launchYouTube(BuildContext context, String stringURL) async {
+    final Uri url = Uri.parse(stringURL);
 
-    if (await canLaunchUrl(url)) {
-      await launchUrl(
-        url,
-        mode: LaunchMode
-            .externalApplication, // Abre no app do YouTube, se disponível
+    try {
+      if (await canLaunchUrl(url)) {
+        await launchUrl(
+          url,
+          mode: LaunchMode
+              .externalApplication, // Abre no app do YouTube, se disponível
+        );
+      } else {
+        throw 'Não foi possível abrir o link';
+      }
+    } catch (e) {
+      // Mostra uma mensagem de erro para o usuário
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erro: $e'),
+          backgroundColor: Colors.red,
+        ),
       );
-    } else {
-      throw 'Não foi possível abrir o link $link';
     }
   }
 }
